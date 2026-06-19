1701d890c120613e3f4c010591e644aeb5663832
Daniel Fisher <daniel.fisher@climate-x.com>
2026-06-19 15:02:26 +0100

    feat: export public API in lib.rs

--- crates/boostlss/src/lib.rs
+++ crates/boostlss/src/lib.rs
@@ -4,6 +4,13 @@ pub mod data;
 pub mod error;
 pub mod family;
 pub mod param;
-pub mod util; // Task 2 // Task 4 // Task 5 // Task 6
+pub mod util;
 
+pub use data::Dataset;
 pub use error::BoostlssError;
+pub use family::gamma::GammaLss;
+pub use family::gaussian::GaussianLss;
+pub use family::nbinomial::NBinomialLss;
+pub use family::student_t::StudentTLss;
+pub use family::Family;
+pub use param::{IdentityLink, Link, LogLink, LogitLink, ParamSpec};

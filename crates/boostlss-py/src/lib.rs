a4851c89c467720df4b17963748a3d104a5ef3cc
Daniel Fisher <daniel.fisher@climate-x.com>
2026-06-19 22:22:15 +0100

    feat: expose BoostLssModel binding with fit and predict methods

--- crates/boostlss-py/src/lib.rs
+++ crates/boostlss-py/src/lib.rs
@@ -1,13 +1,16 @@
 mod family;
 mod learner;
+mod model;
 
 use family::PyFamily;
 use learner::PyLinearLearner;
+use model::BoostLssModel;
 use pyo3::prelude::*;
 
 #[pymodule]
 fn boostlss_py(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_class::<PyFamily>()?;
     m.add_class::<PyLinearLearner>()?;
+    m.add_class::<BoostLssModel>()?;
     Ok(())
 }
